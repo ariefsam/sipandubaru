9c5f4a56087fc8ba92f20156650af52f941c6935
ariefhidayatulloh <ariefhidayatulloh@0b849738-2e13-abe8-6b3c-170a1d1c1628>
2011-06-07 06:32:01 +0000

    Nambah grafik nih

--- gr_perkembangan.php
+++ gr_perkembangan.php
@@ -1,15 +1,42 @@
- <?php
+<?php
+$id_bayi = $_GET['id'];
+$bayi = get_bayi($id_bayi);
+$data = get_berat_tinggi($id);
 
- $umur = 60;
-   for($i = 0; $i<$umur; $i++){
-          $arr_umur[$i][1] = $i;
-          $arr_umur[$i][2] = $i+2.5;
-          $arr_umur[$i][3] = 'Status gizi : Baik';
+$queri = mysql_query("SELECT * FROM pemeriksaan where id_bayi=$id_bayi");
+$umur = 60;
 
-    }
-   $strXML = "<chart bgSWF='images/grafik_kms.jpg' baseFontSize='11' canvasBgAlpha='0' numberPrefix='' xAxisName='Umur (bulan)' yAxisName='berat' yAxisMaxValue='30' xAxisMaxValue='60'decimals='1' >";
-   //Convert data to XML and append
-   foreach ($arr_umur as $arSubData2)
-      $strXML .= "<set label='" . $arSubData2[1] . "' value='" . $arSubData2[2] . "' tooltext=' " . $arSubData2[3] . " '/>";
-   $strXML .= "</chart>";
+
+
+for($a=0; $a<=36; $a++)
+{
+    $arrData[$a][1] = $a;
+    
+        if($data[$a]['berat']){
+
+            $arrData[$a][2] = $data[$a]['berat'];
+            $arrData[$a][3] = "Bagus";
+        }
+        else
+        {
+           $arrData[$a][2] = "";
+           $arrData[$a][3] = "";
+        }
+    
+
+}
+
+print_r($arrData);
+
+//while($q = mysql_fetch_array($queri)){
+//      $arr_umur[$i][1] = $q['berat'];
+//      $arr_umur[$i][2] = $i+2.5;
+//      $arr_umur[$i][3] = 'Status gizi : Baik';
+//      $i++;
+//}
+$strXML = "<chart bgSWF='images/grafik_kms.jpg' baseFontSize='11' canvasBgAlpha='0' numberPrefix='' xAxisName='Umur (bulan)' yAxisName='berat' yAxisMaxValue='30' xAxisMaxValue='60'decimals='1' >";
+//Convert data to XML and append
+foreach ($arrData as $arSubData2)
+  $strXML .= "<set label='" . $arSubData2[1] . "' value='" . $arSubData2[2] . "' tooltext=' " . $arSubData2[3] . " '/>";
+$strXML .= "</chart>";
 ?>
--- grafik.php
+++ grafik.php
@@ -14,8 +14,8 @@ include 'gr_perkembangan.php';
     <div id="content-grafik">
         <h1>Grafik Perkembangan</h1> <div id="nama_anak">Alfa Nugraha</div>
         Kembali
-        <div style='overflow:auto; width:ancho; height:700px; width: 960px;'>
-            <?php
+        <div style='overflow:auto; width:ancho; height:750px; width: 960px;'>
+            <?php echo "hhh" . $strXML;
                   echo renderChart("chart/Line.swf", "", $strXML, "statusGizi",  1331,700, false, false); ?>
                     </div>
     </div>
