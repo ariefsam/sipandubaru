5552f6c6523a212021eb9d96825a983e053ebfab
ariefhidayatulloh <ariefhidayatulloh@0b849738-2e13-abe8-6b3c-170a1d1c1628>
2011-06-08 16:22:31 +0000

    Edit Biodata. Ada Log Arief juga disana. Ditambahin development bundle buat JQuery UI. Dicopy semuanya aja coz males misah2in

--- gr_jk.php
+++ gr_jk.php
@@ -1,9 +1,11 @@
  <?php
    $arrJK[0][1] = "Perempuan";
    $arrJK[1][1] = "Laki-Laki";
+   $jumlah_laki = mysql_fetch_array(mysql_query("SELECT * FROM bayi WHERE jenis_kelamin='L'"));
+   $jumlah_perempuan = mysql_fetch_array(mysql_query("SELECT * FROM bayi WHERE jenis_kelamin='P'"));
 
-   $arrJK[0][2] = 13;
-   $arrJK[1][2] = 36;
+   $arrJK[0][2] = $jumlah_perempuan[0];
+   $arrJK[1][2] = $jumlah_laki[0];
    $JK = "<chart caption='Peserta Posyandu Bulan Ini' xAxisName='Umur' baseFontSize='12' yAxisName='Berat' yAxisMinValue='0' yAxisMaxValue='30' adjustDiv='0' numDivLines='0' bgSWF='ffffff' canvasBgAlpha='100' canvasBorderColor='ffffff' canvasBorderThickness='3'>";
    //Convert data to XML and append
    foreach ($arrJK as $arSubData2)
